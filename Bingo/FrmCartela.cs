15ab57d9cd3d827b687ec1040621356191651943
Caio Henrique <caio.h.garcia@unesp.br>
2025-06-19 23:56:00 -0300

    Validação - Cartela

--- Bingo/FrmCartela.cs
+++ Bingo/FrmCartela.cs
@@ -17,6 +17,7 @@ namespace Bingo
         int[][] numeros;
         Label[][] posicoes;
         int sorteados;
+
         public FrmCartela(FrmSorteador sorteador, int id)
         {
             InitializeComponent();
@@ -36,27 +37,50 @@ namespace Bingo
             {
                 new Label[5] { lbl1_1, lbl1_2, lbl1_3, lbl1_4, lbl1_5 },
                 new Label[5] { lbl2_1, lbl2_2, lbl2_3, lbl2_4, lbl2_5 },
-                new Label[5] { lbl3_1, lbl3_2, null, lbl3_4, lbl3_5 },
+                new Label[5] { lbl3_1, lbl3_2, null,    lbl3_4, lbl3_5 },
                 new Label[5] { lbl4_1, lbl4_2, lbl4_3, lbl4_4, lbl4_5 },
                 new Label[5] { lbl5_1, lbl5_2, lbl5_3, lbl5_4, lbl5_5 },
             };
 
             sorteados = 1;
             CriarCartela();
-
         }
 
         void CriarCartela()
         {
             Random r = new Random();
+            int[] usados = new int[24]; // Para armazenar os números únicos sorteados
+            int totalUsados = 0;
 
-            for (int i = 0; i < 5; i++) // i=> coluna E indicador de intervalo
+            for (int i = 0; i < 5; i++) // colunas
             {
-                for (int j = 0; j < 5; j++) // j => linha
+                for (int j = 0; j < 5; j++) // linhas
                 {
-                    if (!(i == j && i == 2))
+                    if (!(i == 2 && j == 2)) // Ignora o centro (posição livre)
                     {
-                        int n = r.Next(1, 16) + i * 15;
+                        int n;
+                        bool repetido;
+
+                        do
+                        {
+                            n = r.Next(1, 16) + i * 15; // Número dentro da faixa da coluna
+                            repetido = false;
+
+                            // Verifica se o número já foi usado
+                            for (int k = 0; k < totalUsados; k++)
+                            {
+                                if (usados[k] == n)
+                                {
+                                    repetido = true;
+                                    break;
+                                }
+                            }
+
+                        } while (repetido);
+
+                        usados[totalUsados] = n;
+                        totalUsados++;
+
                         numeros[i][j] = n;
                         posicoes[i][j].Text = n.ToString();
                     }
@@ -69,24 +93,19 @@ namespace Bingo
             int i = (numero - 1) / 15;
 
             for (int j = 0; j < 5; j++)
+            {
+                if (numeros[i][j] == numero)
                 {
-                   if (numeros[i][j] == numero)
-                   {
-                        posicoes[i][j].BackColor = Color.Yellow;
-                        sorteados++;
+                    posicoes[i][j].BackColor = Color.Yellow;
+                    sorteados++;
 
-                        if (sorteados == 25)
-                        {
-                            lblVitoria.Visible = true;
-                            sorteador.AnunciarVitoria(this);
-                        }
-
-                   }
-
+                    if (sorteados == 25)
+                    {
+                        lblVitoria.Visible = true;
+                        sorteador.AnunciarVitoria(this);
+                    }
+                }
             }
-
         }
-
     }
-
 }
